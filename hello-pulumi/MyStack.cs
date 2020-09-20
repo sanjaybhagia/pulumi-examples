524fc5c60b017b7564e2141b33f85e05c6310e30
Sanjay Bhagia <sanjay.bhagia@kloud.com.au>
2020-09-21 09:31:21 +1000

    added pulumi-hello

---
+++ hello-pulumi/MyStack.cs
@@ -0,0 +1,29 @@
+using Pulumi;
+using Pulumi.Azure.Core;
+using Pulumi.Azure.Storage;
+
+class MyStack : Stack
+{
+    public MyStack()
+    {
+        // Create an Azure Resource Group
+        var resourceGroup = new ResourceGroup("resourceGroup");
+
+        // Create an Azure Storage Account
+        var storageAccount = new Account("storage", new AccountArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            AccountReplicationType = "LRS",
+            AccountTier = "Standard",
+            Tags =  {
+                { "Environment", "Dev" }
+            }
+        });
+
+        // Export the connection string for the storage account
+        this.ConnectionString = storageAccount.PrimaryConnectionString;
+    }
+
+    [Output]
+    public Output<string> ConnectionString { get; set; }
+}
