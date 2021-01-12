d035014aa852ff2d68a8958d5036037a5cf50ae4
Sanjay Bhagia <sanjay.bhagia@kloud.com.au>
2021-01-12 18:07:25 +1100

    pulumi configurations

--- hello-pulumi/MyStack.cs
+++ hello-pulumi/MyStack.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Pulumi;
 using Pulumi.Azure.Core;
 using Pulumi.Azure.Storage;
@@ -6,13 +7,40 @@ class MyStack : Stack
 {
     public MyStack()
     {
+        // Read configurations
+        var config = new Config();
+
+        var project = Deployment.Instance.ProjectName;
+        var stack = Deployment.Instance.StackName;
+
+        //This will show the information on console when you are using pulumi preview or pulumi up
+        Pulumi.Log.Info($"Project:{project}");
+        Pulumi.Log.Info($"Stack:{stack}");
+
+        var location = config.Require("location");
+        Pulumi.Log.Info($"location:{location}");
+
+        // Fetch Structured Configuration
+        var data = config.RequireObject<JsonElement>("data");
+        Pulumi.Log.Info($"Active: {data.GetProperty("active")}");
+        Pulumi.Log.Info($"Nums[2] number?: {data.GetProperty("nums")[2]}");
+
         // Create an Azure Resource Group
-        var resourceGroup = new ResourceGroup("resourceGroup");
+        var resourcesName = config.Require("resourcesName");
+        var resourceGroupName = $"rg-{resourcesName}-{stack}";
+
+        var resourceGroup = new ResourceGroup("resourceGroup", new ResourceGroupArgs()
+        {
+            Name = resourceGroupName,
+            Location = location
+        });
 
         // Create an Azure Storage Account
+        var storageAccountName = $"str{resourcesName}{stack}";
         var storageAccount = new Account("storage", new AccountArgs
         {
             ResourceGroupName = resourceGroup.Name,
+            Name = storageAccountName,
             AccountReplicationType = "LRS",
             AccountTier = "Standard",
             Tags =  {
